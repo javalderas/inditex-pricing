a15f149beafb84bfa7554fb897147f5bc094e9a3
Juan Antonio Valderas Molina <juanantonio@Juans-MacBook-Pro.local>
2025-09-14 15:20:24 +0200

    First Commit, app run base

---
+++ src/main/kotlin/com/gft/inditex_pricer/InditexPricerApplication.kt
@@ -0,0 +1,11 @@
+package com.gft.inditex_pricer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InditexPricerApplication
+
+fun main(args: Array<String>) {
+	runApplication<InditexPricerApplication>(*args)
+}
---
+++ src/test/kotlin/com/gft/inditex_pricer/InditexPricerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gft.inditex_pricer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InditexPricerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
